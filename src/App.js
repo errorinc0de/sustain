bbac02615edb1dddf1707f41bf7dba912d49b352
Soham Sarkar <sohamsarkarv1.0@gmail.com>
2021-06-16 16:59:28 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import { AuthProvider } from './context/AuthProvider';
+
+// Common People or Users Components Imports
+import PeopleLogin from './routes/people/login'
+// End
+
+// Government Components Imports
+
+// End
+
+// Shops Components Imports
+
+// End
+
+// Company Components Imports
+
+// End
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <AuthProvider>
+        <Switch>
+          <Route exact path="/" component={PeopleLogin} />
+          <Route exact path="/peopleLogin" component={PeopleLogin} />
+        </Switch>
+      </AuthProvider>
+    </Router>
   );
 }
 
