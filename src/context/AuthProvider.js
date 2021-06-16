8438952d9e9be402288e5c8b0f35ac588bc96122
Soham Sarkar <sohamsarkarv1.0@gmail.com>
2021-06-16 16:53:42 +0530

    Initial

---
+++ src/context/AuthProvider.js
@@ -0,0 +1,142 @@
+import React, { useContext, useState, useEffect } from "react"
+import { auth, db } from "../firebase"
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+export function AuthProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState()
+    const [loading, setLoading] = useState(true)
+  
+    function signup(email, password, name) {
+        return new Promise((resolve, reject) => {
+            auth.createUserWithEmailAndPassword(email, password).then(function(result) {
+            const user = auth.currentUser;
+            user.updateProfile({
+                displayName: name
+            }).then((result)=> {
+                db.collection("users").doc(user.uid).set({
+                    displayName: name,
+                    uid: user.uid,
+                    email: email
+                }).then(()=>{
+                    resolve()
+                })
+                .catch(error => reject("error creating profile"));
+            })
+            .catch(error => reject(error));
+            })
+            .catch(error => reject(error));
+        })
+    }
+  
+    function createUser(payload)
+    {
+        db.collection("users").doc(payload.user_id).onSnapshot((doc)=>{
+    
+            if(!doc.exists)
+            {
+                db.collection("users").doc(payload.user_id).set({
+                    displayName:"",
+                    uid:payload.user_id,
+                    phoneNumber:payload.identifier
+                })
+            } else {
+                setCurrentUser(doc.data())
+            }
+        })
+    }
+
+    function login(email, password) {
+        return new Promise((resolve, reject) => {
+          db.collection("users").where("email","==",email).onSnapshot((docs)=>{
+            if(!docs.empty)
+            {
+              docs.forEach(doc=>{
+                if(doc.exists)
+              {
+                  if(doc.data().isGovernment === undefined && doc.data().isShop === undefined && doc.data().isCompany === undefined) 
+                  {
+                    auth.signInWithEmailAndPassword(email, password).then(() => {
+                      resolve()
+                    })
+                    .catch(error => reject(error));
+                  }
+                  else
+                  {
+                    reject({message:"Not registered as an user"})
+                  }
+              }
+              else
+              {
+                reject({message:"no such account exists"})
+              }
+              })
+            }
+            else
+            {
+              reject({message:"no such account exists"})
+            }
+          })
+        })
+    }
+
+    function logout() {
+        sessionStorage.clear();  
+            setCurrentUser()
+        return auth.signOut()
+      }
+    
+      function resetPassword(email) {
+        return auth.sendPasswordResetEmail(email)
+      }
+    
+      function updateEmail(email) {
+        return currentUser.updateEmail(email)
+      }
+    
+      function updatePassword(password) {
+        return currentUser.updatePassword(password)
+      }
+    
+      useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+          if(user && user.uid)
+          {
+            db.collection("users").doc(user.uid).get().then((docs)=>{
+              if(!docs.empty)
+                {
+                  setCurrentUser(docs.data())
+                  setLoading(false)
+                }
+              })
+          }else
+            setLoading(false)
+          
+        })
+    
+        return unsubscribe
+      }, [])
+    
+    
+    
+      const value = {
+        currentUser,
+        login,
+        signup,
+        logout,
+        resetPassword,
+        updateEmail,
+        updatePassword,
+        createUser
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+          {!loading && children}
+        </AuthContext.Provider>
+    )
+}
---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app"
+import "firebase/firestore"
+import "firebase/storage"
+import "firebase/auth"
+import * as geofirestore from 'geofirestore';
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyBJ0F-0F6DirzrqEffNga1LwAyV9_eGPoM",
+    authDomain: "sustain-b7e1a.firebaseapp.com",
+    projectId: "sustain-b7e1a",
+    storageBucket: "sustain-b7e1a.appspot.com",
+    messagingSenderId: "93205689116",
+    appId: "1:93205689116:web:fa2e5bebc195fe2c9340d9",
+    measurementId: "G-9TD82F04MM"
+})
+
+export const auth = app.auth()
+export const db = firebase.firestore();
+export var provider = new firebase.auth.GoogleAuthProvider();
+export const firebasevalue = firebase.firestore.FieldValue;
+export const storageRef = firebase.storage().ref();
+export const timestamp = firebase.firestore.FieldValue.serverTimestamp()
+export const GeoFirestore = geofirestore.initializeApp(db);
+export const geoRef = firebase.firestore
+export default app
